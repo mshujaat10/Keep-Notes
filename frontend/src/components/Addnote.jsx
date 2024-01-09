cb2301c13c0ca8fb08f04d52acc539dce74836b9
mshujaat10 <muhammadshujaat56@gmail.com>
2024-01-09 21:08:56 +0500

    Dark mode added :)

--- frontend/src/components/Addnote.jsx
+++ frontend/src/components/Addnote.jsx
@@ -17,20 +17,19 @@ const Addnote = (props) => {
     addNote(note.title, note.description);
     setNote({ title: "", description: "" })
   };
-
-  return (
-    <>
-      <div className="d-flex justify-content-center">
-        <div className="mt-5 add-note-inputs rounded py-2">
-          <textarea className={`font-lg form-control rounded-0 border-0 text-${props.mode === "dark" ? "light" : "dark"} bg-${props.mode === "light" ? "white" : "dark"}`} placeholder='Title' id="exampleFormControlTextarea1" name='title' value={note.title} rows="1" onChange={OnChange}></textarea>
-          <textarea className={`font form-control rounded-0 border-0 text-${props.mode === "dark" ? "light" : "dark"} bg-${props.mode === "light" ? "white" : "dark"}`} placeholder='Take a note..' id="exampleFormControlTextarea2" name='description' value={note.description} rows="1" onChange={OnChange}></textarea>
-          <div className="text-end">
-            <button type="submit" onClick={AddNote} className={`bg-${props.mode} note-submit-btn rounded-0 py-1 px-4 text-${props.mode=="light"?"dark":"light"} btn font`}>Add Note</button>
-          </div>
+return (
+  <>
+    <div className="d-flex justify-content-center">
+      <div className="mt-5 add-note-inputs rounded py-2">
+        <textarea className={`font-lg form-controls w-100 px-2 rounded-0 border-0 text-${props.mode === "dark" ? "light" : "dark"} bg-${props.mode === "light" ? "white" : "dark"}`} placeholder='Title' id="exampleFormControlTextarea1" name='title' value={note.title} rows="1" onChange={OnChange}></textarea>
+        <textarea className={`font form-controls w-100 px-2 rounded-0 border-0 text-${props.mode === "dark" ? "light" : "dark"} bg-${props.mode === "light" ? "white" : "dark"}`} placeholder='Take a note..' id="exampleFormControlTextarea2" name='description' value={note.description} rows="1" onChange={OnChange}></textarea>
+        <div className="text-end">
+          <button type="submit" onClick={AddNote} className={`bg-${props.mode} note-submit-btn rounded-0 py-1 px-4 text-${props.mode == "light" ? "dark" : "light"} btn font`}>Add Note</button>
         </div>
       </div>
-    </>
-  );
+    </div>
+  </>
+);
 };
 
 export default Addnote;
